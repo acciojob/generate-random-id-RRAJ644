55dd66df45ac13708b79fb79d32168bcb50a72ba
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-04 11:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function makeid(l) {
   // write your code here
+	const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	let ans = ''
+
+	for(let i=0; i<str.length; i++){
+		ans = ans + str.charAt(Math.floor(Math.random()*str.length))
+	}
+
+	return ans
+	
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+// const l = prompt("Enter a number.");
 alert(makeid(l));
